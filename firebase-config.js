8df519afcb75af5db15e8a73dc54499c9578ea46
MarsMobileServices <mrsmrmobileservices@gmail.com>
2026-06-12 13:38:34 -0400

    try 2

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
+  getAuth
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import { getAuth }
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
   getFirestore,
   doc,
-  getDoc
-}
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+  getDoc,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFK-JCo-TwyD8wtvuxK9XBhOWRcw7K1xw",
@@ -40,22 +35,4 @@ export {
   getDocs,
   addDoc,
   deleteDoc
-};
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCFK-JCo-TwyD8wtvuxK9XBhOWRcw7K1xw",
-  authDomain: "marsmobileservices.firebaseapp.com",
-  projectId: "marsmobileservices",
-  storageBucket: "marsmobileservices.firebasestorage.app",
-  messagingSenderId: "187685450627",
-  appId: "1:187685450627:web:0056af1b087dcc8348bcb6"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export {
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
 };
